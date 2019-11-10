68bd7c8b3ff1441887bb4d9eb131f557b6733f85
kenmakonao <wearekenchan@gmail.com>
2019-11-10 10:36:52 +0000

    first commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = Refile.secret_key = '68150c806556ae47178f92555c98e49a954ba7c156c69cae545a29d13965fd280d725faf22ae441356d6993946831382faa2d21e9ab49832aa981602174ef33f'
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root 'post_images#index'
+resources :post_images, only: [:new, :create, :index, :show]
+	resource :favorites, only: [:create, :destroy]
+	resource :post_comments, only: [:create, :destroy]
+    resources :users, only: [:show, :edit, :update]
+end
